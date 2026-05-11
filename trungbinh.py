7c88506ddb461a363ba08834381e655ce295a755
thao06022007-cyber <thao06022007@gmail.com>
2026-05-12 00:07:43 +0700

    Add files via upload

---
+++ trungbinh.py
@@ -0,0 +1,16 @@
+# 1. Tạo một danh sách các số
+danh_sach_so = [10, 20, 30, 40, 50]
+
+# 2. Tính tổng các số trong danh sách
+tong = sum(danh_sach_so)
+
+# 3. Đếm xem có bao nhiêu số trong danh sách
+so_luong = len(danh_sach_so)
+
+# 4. Tính trung bình
+giatri_trungbinh = tong / so_luong
+
+# 5. In kết quả
+print("Danh sách số của bạn là:", danh_sach_so)
+print(f"Giá trị trung bình của dãy số là: {giatri_trungbinh}")
+
